c6ace949735628b1691a0ae9d7b2ffdfca3a18d0
AnDr10wA <45951252+AnDr10wA@users.noreply.github.com>
2023-07-03 19:12:11 +0300

    Update project controller

--- src/main/java/com/kata/webSpring/controller/UserController.java
+++ src/main/java/com/kata/webSpring/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.kata.webSpring.controller;
 
 import com.kata.webSpring.model.User;
-import com.kata.webSpring.service.UserServiceImpl;
+import com.kata.webSpring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,16 +15,16 @@ import java.util.List;
 @Controller
 @RequestMapping("/users")
 public class UserController {
-    private UserServiceImpl userServiceImpl;
+    private UserService userService;
 
     @Autowired
-    public UserController(UserServiceImpl userServiceImpl) {
-        this.userServiceImpl = userServiceImpl;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @GetMapping
     public String findAll(Model model){
-        List<User> users = userServiceImpl.findAll();
+        List<User> users = userService.findAll();
         model.addAttribute("users", users);
         return "user-list";
     }
@@ -35,23 +35,23 @@ public class UserController {
 
     @PostMapping("/create")
     public String createUser(User user){
-        userServiceImpl.saveUser(user);
+        userService.saveUser(user);
         return "redirect:/users";
     }
     @GetMapping("delete/{id}")
     public String deleteUser(@PathVariable("id")  Long id){
-        userServiceImpl.deleteById(id);
+        userService.deleteById(id);
         return "redirect:/users";
     }
     @GetMapping("update/{id}")
     public String updateUserForm(@PathVariable("id")  Long id, Model model){
-        User user = userServiceImpl.findById(id);
+        User user = userService.findById(id);
         model.addAttribute("user", user);
         return "/user-update";
     }
     @PostMapping("/update")
     public String updateUser(User user){
-        userServiceImpl.saveUser(user);
+        userService.saveUser(user);
         return "redirect:/users";
     }
 
